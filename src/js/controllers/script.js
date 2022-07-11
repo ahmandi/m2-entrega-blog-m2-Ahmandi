009333d7b66865876f100de533eee6bd1e99c14c
Amanda <93018313+Ahmandi@users.noreply.github.com>
2022-07-11 12:14:23 -0300

    Update script.js

--- src/js/controllers/script.js
+++ src/js/controllers/script.js
@@ -2,21 +2,22 @@ import userRequest from "./cadastro.controller.js";
 import Login from "./login.controller.js";
 import usersOperations from "./users.controller.js"
 
-// const cadastro = await userRequest.cadastro({
-//     "username": "Mandicita",
-//     "email": "mildmandydono@gmail.com",
-//     "avatarUrl": "https://github.com/phmc99.png",
-//     "password": "Teste123"  
-// })
+ const cadastro = await userRequest.cadastro({
+     "username": "Mandicita",
+     "email": "mildmandydono@gmail.com",
+     "avatarUrl": "https://github.com/phmc99.png",
+     "password": "Teste123"  
+ })
 
-// const login = await Login.login({
-//     "email": "mildmandydono@gmail.com",
-//     "password": "Teste123"
-// })
+ const login = await Login.login({
+     "email": "mildmandydono@gmail.com",
+     "password": "Teste123"
+ })
 
-// const userInfo = await usersOperations.listUserbyId(2)
-// const usersPost = await usersOperations.showPosts()
-//const postagem = await usersOperations.post({content: 'postagem unica e original'})
+ const userInfo = await usersOperations.listUserbyId(2)
+ const usersPost = await usersOperations.showPosts()
 
-//console.log(login, 'login success')
-//console.log(postagem)
+console.log(cadastro)
+console.log(login, 'login success')
+console.log(userInfo)
+console.log(usersPost)
